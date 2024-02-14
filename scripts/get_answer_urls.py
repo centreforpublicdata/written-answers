f5f82fb8a20411f22294dfe4711755be71e8f06d
Anna Powell-Smith <annapowellsmith@gmail.com>
2024-02-14 14:19:18 +0000

    Rename scripts

--- scripts/get_answer_urls.py
+++ scripts/get_answer_urls.py
@@ -1,19 +1,11 @@
 import csv
-from datetime import date
-import re
 import requests
-import sys
-import time
-from pyquery import PyQuery as pq
-from pprint import pprint
 
-YEAR = 2022
+from pyquery import PyQuery as pq
+
+
 BASE_URL = 'https://www.theyworkforyou.com'
 
-
-# Get all written answers from TWFY for a year.
-# If you want to look at one particular date, use the Parliament site:
-# https://questions-statements.parliament.uk/written-questions?AnsweredFrom=12%2F12%2F2019&AnsweredTo=01%2F01%2F2020&House=Commons
 def get_answer_urls_for_date(date_url, date_answered, writer):
 	response = requests.get(date_url)
 	doc = pq(response.text)
@@ -26,10 +18,10 @@ def get_answer_urls_for_date(date_url, date_answered, writer):
 def get_answer_urls(year):
 	print("Getting URLs for %s" % year)
 	url_header = ['url', 'date_answered']
-	writer = csv.DictWriter(open('./data/written_answer_urls_%s.csv' % year, 'w'), \
+	writer = csv.DictWriter(open('../data/urls/written_answer_urls_%s.csv' % year, 'w'), \
 		fieldnames=url_header)
 	writer.writeheader()
-	year_url = 'https://www.theyworkforyou.com/wrans/?y=%s' % year
+	year_url = '%s/wrans/?y=%s' % (BASE_URL, year)
 	response = requests.get(year_url)
 	doc = pq(response.text)
 	links = doc('.calendar a')
@@ -38,5 +30,15 @@ def get_answer_urls(year):
 		date_answered = href.split('/?d=')[1]
 		get_answer_urls_for_date(BASE_URL + href, date_answered, writer)
 
-# TODO make this a command-line argument
-get_answer_urls(YEAR)
+def main(args):
+	get_answer_urls(args.year)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(\
+    	description="Get URLs of written answers from TWFY for a given year.")
+    parser.add_argument('-y', '--year', help='Year to process',
+    	required=True)
+    args = parser.parse_args()
+
+    main(args)
--- scripts/scrape_urls.py
+++ scripts/scrape_urls.py
@@ -1,29 +1,29 @@
 import csv
 from datetime import date
+import os
 import re
 import requests
-import sys
 import time
-from pyquery import PyQuery as pq
-from pprint import pprint
 
-'''
-Fetch all written answers from a given year, and dump them into a CSV file.
-Here's an example: https://www.theyworkforyou.com/wrans/?id=2019-12-20.326.h
-We will scrape the following fields into our CSV file:
-- Title
-- Date
-- Department being asked
-- Question text (NB there may be multiple questions)
-- Answer text
-- Whether it has an attachment
-- Reader vote breakdown, in case this is useful for spotting unanswered questions
-'''
+from pyquery import PyQuery as pq
+
 
 BASE_URL = 'https://www.theyworkforyou.com'
 
-# Now we have all the URLs, so scrape each individual Written Answer.
 def scrape_answer(url, date_submitted, date_answered, question_id):
+	'''
+	Scrape an individual Written Answer from a TWFY URL.
+	Here's an example: 
+	https://www.theyworkforyou.com/wrans/?id=2019-12-20.326.h
+	We will scrape the following fields:
+	- Title
+	- Date
+	- Department being asked
+	- Question text (NB there may be multiple questions)
+	- Answer text
+	- Whether it has an attachment
+	- Reader vote breakdown
+	'''
 	response = requests.get(url)
 	doc = pq(response.text)
 	data = {}
@@ -34,7 +34,6 @@ def scrape_answer(url, date_submitted, date_answered, question_id):
 	data['department'] = header[0].replace(" written question", "").strip()
 	data['date_submitted'] = date_submitted # header[1].replace(" answered on", "").replace('.', '').strip()
 	data['date_answered'] = date_answered
-	# print(title, department, date_submitted)
 
 	question0 = doc('#g%s\\.q0' % question_id)
 	question0q = pq(question0)
@@ -49,24 +48,17 @@ def scrape_answer(url, date_submitted, date_answered, question_id):
 			temp = t(".debate-speech__content")
 			question_text += temp.text().replace('\n', '') + "\n\n"
 	data["question_text"] = question_text
-	# print(question_speaker, question_position)
-	# print(question_text)
 
 	answer = doc('#g%s\\.r0' % question_id)
 	ans = pq(answer)
 	data['answer_speaker'] = ans('.debate-speech__speaker__name').text()
 	data['answer_position'] = ans('.debate-speech__speaker__position').text()
 	data['answer_text'] = ans('.debate-speech__content').text().replace('\n', '')
-	# print(answer_speaker, answer_position)
-	# print(answer_text)
-
 	question_answered = doc('.question-answered-result__vote-text')
 	votes_answered = question_answered.eq(0).text().\
 		replace(" person thinks so", '').replace("people think so", "").strip()
 	votes_notanswered = question_answered.eq(1).text().\
 		replace(" person thinks not", '').replace("people think not", "").strip()
-	# print(votes_answered)
-	# print(votes_notanswered)
 	data['votes_answered'] = votes_answered
 	data['votes_notanswered'] = votes_notanswered
 	if votes_answered and votes_notanswered:
@@ -75,62 +67,63 @@ def scrape_answer(url, date_submitted, date_answered, question_id):
 		data['votes_diff'] = 0
 
 	data['attachment'] = doc('.qna-result-attachments-container').text()
-	# pprint(data)
-	# sys.exit()
 	return data
 
-def get_answers(url_file, params, outfile_name):
-	print("Scraping answers")
-
+def get_answers(year):
+	'''
+	Process a list of input URLs, and any data we have already scraped,
+	and store to an output file.
+	'''
+	url_file = "../data/urls/written_answer_urls_%s.csv" % year
+	outfile = "../data/raw_answers/output_%s.csv" % year
 	today = date.today()
-	if not outfile_name:
-		outfile_name = './data/output_%s.csv' % today.strftime("%Y-%m-%d")
 
-	# Check which URLs you have already scraped, and load them into local data.
+	# First check for answers we have already scraped, and
+	# rename the output file if necessary.
 	previously_scraped_urls = []
-	# reader = csv.DictReader(open("./data/output_%s.csv" % params, "r"))
-	# for row in reader:
-	# 	previously_scraped_urls.append({row['url']: row})
+	if os.path.exists(outfile):
+		reader = csv.DictReader(open(outfile, "r"))
+		for row in reader:
+			previously_scraped_urls.append({row['url']: row})
+		outfile = '../data/raw_answers/output_%s_%s.csv' % \
+			(year, today.strftime("%Y-%m-%d"))
+	if previously_scraped_urls:
+		print(len(previously_scraped_urls), 'previously scraped URLs found')
 
-	# Prepare your output file. TODO: improve the naming.
 	header = [
 		'url', 'title', 'department', 'date_submitted', 'date_answered',
 		'question_speaker', 'question_position', 'question_text',
 		'answer_speaker', 'answer_position', 'answer_text',
 		'votes_answered', 'votes_notanswered', 'votes_diff', 'attachment'
 	]
-	writer = csv.DictWriter(open(outfile_name, 'w'), fieldnames=header)
+	writer = csv.DictWriter(open(outfile, 'w'), fieldnames=header)
 	writer.writeheader()
-
-	# Open your URL file.
 	reader = csv.DictReader(open(url_file, 'r'))
 	counter = 0
 	for row in reader:
+		counter += 1
 		url = row['url']
 		if url in previously_scraped_urls:
 			writer.writerow(urls[url])
 		else:
-			# print('SCRAPING', url)
 			date_answered = row['date_answered']
 			date_submitted = re.search(r'id=(.*?)\.', url).group(1)
 			question_id = re.search(r'id=(.*?)\.(.*?)\.', url).group(2)
 			if (counter % 100) == 0:
 				print(counter)
 			row = scrape_answer(url, date_submitted, date_answered, question_id)
-			# pprint(row)
 			writer.writerow(row)
 			# time.sleep(2)
-		counter += 1
-		# print()
-		# print()
 
+def main(args):
+	get_answers(args.year)
 
-# TODO: Make filenames a command-line argument
-YEAR = 2022
-params = {
-	"department": "",
-	"year": YEAR
-}
-url_file = "./data/written_answer_urls_%s.csv" % YEAR
-get_answers(url_file, params, "./data/output_%s.csv" % YEAR)
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser(\
+		description="Fetch all written answers from a given year from TWFY.")
+	parser.add_argument('-y', '--year', help='Year to process',
+		required=True)
+	args = parser.parse_args()
 
+	main(args)
